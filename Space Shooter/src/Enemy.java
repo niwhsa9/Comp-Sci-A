185780b81d82ab1886583f820ad85ada080105d1
Ashwin Gupta <ashwingupta2000@gmail.com>
2020-02-04 11:10:13 -0800

    Add files via upload

--- Space Shooter/src/Enemy.java
+++ Space Shooter/src/Enemy.java
@@ -1,6 +1,13 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
 
 public class Enemy extends GameObject {
 	int level = 1;
@@ -9,25 +16,50 @@ public class Enemy extends GameObject {
 	double prevBullet = 0;
 	Missile m;
 	ArrayList<GameObject> bullets = new ArrayList<GameObject>();
-
+	BufferedImage img;
 	
 	Enemy(double x, double y, double w, double h, int level) {
 		super(x, y, w, h);
 		this.level = level;
 		this.color = Color.MAGENTA;
 		// TODO Auto-generated constructor stub
+		try {
+			Random rn = new Random();
+			int r= rn.nextInt(3);
+			img = ImageIO.read(new File("images/alien" + r+".png"));
+			if(r == 0) color = Color.green;
+			if(r == 1) color = Color.red;
+			if(r == 2) color = Color.red;
+
+		}
+		catch(Exception e) {
+			System.out.println("failed to load");
+		}
 	}
 	double t = 0;
 	
 	boolean enterFlag = false;
 	
 	public void paintComponent(Graphics g) {
-		
-		if(isAlive)	super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		if(isAlive)	{
+			//super.paintComponent(g);
+			
+			
+			
+			
+			AffineTransform transform = new AffineTransform();
+			transform.rotate(phi, centerX(), centerY());
+			AffineTransform original = g2d.getTransform();
+			g2d.transform(transform);
+			g2d.drawImage(img, (int)x, (int)y, (int)width, (int)height, null);
+			g2d.setTransform(original);
+		}
 		if(m != null) m.paintComponent(g);
 		for(int i = 0; i < bullets.size(); i++) {
 			bullets.get(i).paintComponent(g);
 		}
+		
 	}
 
 	
@@ -70,7 +102,7 @@ public class Enemy extends GameObject {
 				if(t - prevBullet > 1.0) {
 					prevBullet = t;
 					GameObject bullet = new GameObject(x, y, 10, 5);
-					bullet.theta = Math.atan2(TestScene.ship.y - y, TestScene.ship.x - x);
+					bullet.theta = Math.atan2(TestScene.ship.y - y, TestScene.ship.x - x) + (Math.random()-0.5)*Math.PI/4;
 					bullet.speed = 300;
 					bullet.phi = bullet.theta;
 					bullet.color = Color.blue;
@@ -98,7 +130,7 @@ public class Enemy extends GameObject {
 
 			}
 		}
-		phi = theta; 
+		phi = theta + Math.PI/2; 
 		}
 	}
 	
